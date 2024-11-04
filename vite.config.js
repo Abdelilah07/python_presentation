ce4102190a132a51f25bd3121d86a92c99f22397
Abdelilah07 <mabroukabdelilah07@gmail.com>
2024-11-04 10:26:37 +0100

    Updating the vite.config file

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'docs',
+  },
+  base: '/python_presentation', // Set to the name of your GitHub repository
+});
